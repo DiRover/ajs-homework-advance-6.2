e5d6ea3b07bd8316207ad1206ab56ea15c1602a2
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-07-09 23:02:23 +0300

    Initial commit

---
+++ game.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-param-reassign */
+const character = { // заданный объект
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+// функция получения описания специального удара
+// eslint-disable-next-line max-len
+function getSpecialAttack({ special: arr } = character) { // с помощью диструкции получаем вложенный массив
+  const arrFn = []; // новый массив для соблюдения иммутабельности передаваемых аргументов
+  for (let i = 0; i < arr.length; i += 1) { // прогоняем массив
+    if (!('description' in arr[i])) { // проверяем на присутствие свойства
+      arr[i].description = 'Описание недоступно'; // добавляем, если нет нужного св-ва
+      arrFn.push(arr[i]);// пушаем в новый массив
+    } else {
+      arrFn.push(arr[i]); // пушаем в новый массив
+    }
+  }
+  return arrFn; // возвращаем новый массив
+}
+getSpecialAttack({ special: arr } = character); // вызов функции
---
+++ src/js/__test__/game.test.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import getSpecialAttack from '../game';
+
+test('check special attack', () => {
+  const character = { // заданный объект
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        // <- обратите внимание, описание "засекречено"
+      },
+    ],
+  };
+  const expected = [
+    {
+      id: 8, name: 'Двойной выстрел', icon: 'http://...', description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9, name: 'Нокаутирующий удар', icon: 'http://...', description: 'Описание недоступно',
+    },
+  ];
+  const received = getSpecialAttack();
+  expect(received).toEqual(expected);
+});
